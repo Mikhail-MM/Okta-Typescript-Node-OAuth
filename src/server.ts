131c0f0cc87cf573fca94d7388cc4cdb47d93659
Mikhail Maslyuk <leakymee@bu.edu>
2019-06-30 20:55:40 -0400

    Add Okta Authentication Scheme Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+
+dotenv.config();
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.SERVER_PORT; // default port to listen;
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
